e618f0271c1e09ce0dc46a06c0c52d6a8deaa37d
LuisSauri <luisasauri@gmail.com>
2024-02-08 11:49:26 -0600

    Examen Parcial 1

---
+++ examen parcial 1/examen_parcial_1/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:examen_parcial_1/screen/rutas.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: FirstRoute(),
+  ));
+}
